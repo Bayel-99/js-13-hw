f567e8eedec5476102441b231543a52f7eefadb0
bikiev bayel <vetrovlena42@gmail.com>
2024-02-28 14:48:33 +0600

    hw-17

---
+++ hw-17/script.js
@@ -0,0 +1,69 @@
+alert('Приветсвую')
+function user(a, b) {
+    const result = a + b;
+    console.log(result
+
+    );
+}
+user(8, 8)
+
+
+function getSquare(number) {
+    return number * number
+}
+console.log(getSquare(5));
+
+
+
+
+const not = function (name, hello) {
+    alert(name + hello)
+
+}
+console.log(not('Bayel', ' salam'));
+
+
+function get(a, b) {
+    let restart = a + b;
+    console.log(restart);
+}
+get(23, ' февраль')
+
+
+const nomer = prompt()
+if (nomer % 2 === 0) {
+    console.log('четное');
+} else {
+    console.log('Число нечётное');
+};
+
+
+const showMassage = function (z, x) {
+    if (z > x) {
+        console.log('чон');
+        prompt()
+    } else if (x < z) {
+        console.log('кичине');
+        prompt()
+    }
+
+}
+
+
+
+function узундугу(строка) {
+    return строка.length
+}
+alert(узундугу("Ermamat"));
+
+
+
+
+function привет(аты, жашы) {
+    return (`Привет.${ аты } !Тебе ${ жашы } лет.`) 
+ }
+alert(привет("Bayel", 19));
+
+
+// 
+
